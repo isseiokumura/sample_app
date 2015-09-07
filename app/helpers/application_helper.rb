b6a0231f83302466a77414024390bd579379228f
isseiokumura <issei096@icloud.com>
2015-09-07 02:24:31 +0000

    Finish user registration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,7 +10,7 @@ module ApplicationHelper
     end
   end
   
-    def bootstrap_class_for(flash_type)
+  def bootstrap_class_for(flash_type)
     case flash_type
       when :success
         "alert-success"
@@ -23,5 +23,5 @@ module ApplicationHelper
       else
         flash_type.to_s
     end
-    end
+  end
 end
